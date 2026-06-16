68cb470c237b1f739c16d5147acf23f260ac339c
Hendry William <hendriwilliam29@gmail.com>
2026-06-16 10:54:22 +0700

    SEO

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,12 +1,27 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
-import tailwindcss from '@tailwindcss/vite'
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
-  devtools: { enabled: true },
-  modules: ['@nuxt/fonts'],
-  css: ['./app/assets/css/main.css'],
-  vite: {
-    plugins: [tailwindcss()],
-  },
-})
+	app: {
+		head: {
+			title: "Hendri William",
+			htmlAttrs: {
+				lang: "en",
+			},
+			link: [
+				{
+					rel: "icon",
+					type: "image/x-icon",
+					href: "/favicon.ico",
+				},
+			],
+		},
+	},
+	compatibilityDate: "2025-07-15",
+	devtools: { enabled: true },
+	modules: ["@nuxt/fonts"],
+	css: ["./app/assets/css/main.css"],
+	vite: {
+		plugins: [tailwindcss()],
+	},
+});
